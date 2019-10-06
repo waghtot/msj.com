b45638986fbd2b87a1af21b15bb70b668b164716
waghtot <waghtot@gmail.com>
2019-10-06 10:05:34 +0100

    slide show update

--- app/views/pages/Home.php
+++ app/views/pages/Home.php
@@ -1,3 +1,7 @@
-<?php
-View::partial('carousel', '0');
-?>
+<div class="container-fluid">
+    <div class="row slide-show-setting">
+    <?php
+    View::partial('carousel', '0');
+    ?>
+    </div>
+</div>
--- app/views/pages/partial/carousel.php
+++ app/views/pages/partial/carousel.php
@@ -1,5 +1,7 @@
-<div class="carousel">
-  <div><h1>my content 01</h1></div>
-  <div><h1>my content 02<h1></div>
-  <div><h1>my content 03<h1></div>
+<div class="container slide-show">
+  <div class="carousel">
+    <div><h1>my content 01</h1></div>
+    <div><h1>my content 02<h1></div>
+    <div><h1>my content 03<h1></div>
+  </div>
 </div>
