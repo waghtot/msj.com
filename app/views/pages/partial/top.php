1f24f3168a0b2c2d2a8d42ecdef425bde585dbc4
Mariusz Marciniak <mariusz.marciniak@tuxedomoney.com>
2021-07-26 19:47:34 +0100

    booking form update

--- app/views/pages/partial/top.php
+++ app/views/pages/partial/top.php
@@ -2,12 +2,12 @@
     <div class="row">
         <div class="container">
             <div class="row">
-                <div class="col-8">
+                <div class="col-lg-8 col-md-8 col-sm-4 hidden-xs">
                     <div>
 
                     </div>
                 </div>
-                <div class="col-4 background-clear">
+                <div class="col-lg-4 col-md-4 col-sm-8 col-xs-12 background-clear">
                     <div>
                         <?php require_once('app/views/pages/partial/form-booking.php'); ?>
                     </div>
