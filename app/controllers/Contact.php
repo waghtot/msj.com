b8a4ee4ddb3c93bc4009b058cab275c90da42eb8
Mariusz Marciniak <mariusz.marciniak@tuxedomoney.com>
2021-07-26 18:30:04 +0100

    update enquiry form with customer phone number

--- app/controllers/Contact.php
+++ app/controllers/Contact.php
@@ -1,9 +1,15 @@
 <?php
 
 if(isset($_POST)){
+
     $to      = 'info.msjconst@gmail.com';
     $subject = 'Customer Enquiry';
-    $message = $_POST['uquery'];
+    if(isset($_POST['uphone']) && !empty($_POST['uphone'])){
+        $message = '<p><h4>Phone: </h4>'.$_POST['uphone'].'</p>';    
+    }else{
+        $message = '';
+    }
+    $message .= '<p>'.$_POST['uquery'].'</p>';
     $headers = 'From: ' . $_POST['uemail'] . "\r\n" .
         'Reply-To: '. $_POST['uemail']  . "\r\n" .
         'X-Mailer: PHP/' . phpversion();
